29f8270850a69985b1735847ac5b18acb87b9b2d
Christina Thompson <c.thompsonms@gmail.com>
2014-03-20 13:30:39 -0700

    idk

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,8 +8,10 @@ class ItemsController < ApplicationController
     @item = Item.create(
       cart_id: item_params[:cart],
       product_id: item_params[:product],
-      quantity: item_params[:quantity]
+      quantity: item_params[:quantity],
+      current_price: item_params[:current_price]
       )
+    render "index"
   end
 
   def show
@@ -18,7 +20,8 @@ class ItemsController < ApplicationController
 
   def update
     @item = Item.find(params[:id])
-    @item.update
+    @item.update(item_params)
+    render "show"
   end
 
   def destroy
@@ -28,6 +31,6 @@ class ItemsController < ApplicationController
 
 private
   def item_params
-    params.require(:item).permit(:current_price, :quantity, :cart_id, :product_id)
+    params.require(:item).permit(:current_price, :quantity, :cart, :product)
   end
 end
